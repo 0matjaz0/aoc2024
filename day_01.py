1a87bf6a32c875640c39735327b0e9d05a507757
MV <matjaz.vencelj@ijs.si>
2024-12-03 10:08:27 +0100

    Finish code for part one

--- day_01.py
+++ day_01.py
@@ -1,7 +1,20 @@
-def kos_root():
-    """Historian Hysteria."""
-    
-
+def first_puzzle(filename):
+    """Historian Hysteria part 1"""
+    with open(filename, "r") as f:
+        data = f.readlines()
+    first_list = []
+    second_list = []
+    for line in data:
+        numbers = line.strip().split()
+        first_list.append(int(numbers[0]))
+        second_list.append(int(numbers[1]))
+    first_list.sort()
+    second_list.sort()
+    diff_list = [abs(second_list[i] - first_list[i]) for i in range(len(first_list))]
+    return sum(diff_list)
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    result = first_puzzle("day_01_input_a.txt")
+    print(result)
+
+
