f953f49383618fb2dad2b37ea919b9ce465596fa
MV <matjaz.vencelj@ijs.si>
2024-12-03 12:13:38 +0100

    Start day 3

---
+++ day_03.py
@@ -0,0 +1,20 @@
+"""AOC 2024 day 3"""
+
+
+def first_puzzle(filename):
+    """Mull It Over part 1"""
+    with open(filename, "r") as f:
+        data = f.readlines()
+    sum_of_products = 0
+    for line in data:
+        #
+    return sum_of_products
+
+
+if __name__ == "__main__":
+    result = first_puzzle("day_03_input_a.txt")
+    print(result)
+#    result = second_puzzle("day_02_input_a.txt")
+#    print(result)
+
+############################################
