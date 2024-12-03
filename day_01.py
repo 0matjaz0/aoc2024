c2fd1c8458493350207c81ee060fae6841317951
MV <matjaz.vencelj@ijs.si>
2024-12-03 10:29:03 +0100

    Write code for second part

--- day_01.py
+++ day_01.py
@@ -13,8 +13,24 @@ def first_puzzle(filename):
     diff_list = [abs(second_list[i] - first_list[i]) for i in range(len(first_list))]
     return sum(diff_list)
 
+def second_puzzle(filename):
+    """Historian Hysteria part 2"""
+    with open(filename, "r") as f:
+        data = f.readlines()
+    first_list = []
+    second_list = []
+    similarity_list = 0  
+    for line in data:
+        numbers = line.strip().split()
+        first_list.append(int(numbers[0]))
+        second_list.append(int(numbers[1]))
+    similarity_list = [second_list.count(first_list[i]) for i in range(len(first_list))]
+    return sum(similarity_list)
+
 if __name__ == "__main__":
     result = first_puzzle("day_01_input_a.txt")
+    print(result)
+    result = second_puzzle("day_01_input_b.txt")
     print(result)
 
 
