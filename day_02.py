b863ac994dafb0f7737679111ce8978f46972956
MV <matjaz.vencelj@ijs.si>
2024-12-03 11:30:26 +0100

    Finish part 1, start part 2

--- day_02.py
+++ day_02.py
@@ -5,16 +5,34 @@ def first_puzzle(filename):
     """Red-Nosed Reports part 1"""
     with open(filename, "r") as f:
         data = f.readlines()
-    first_list = []
-    second_list = []
+    total_safe = 0
     for line in data:
-        numbers = line.strip().split()
-        first_list.append(int(numbers[0]))
-        second_list.append(int(numbers[1]))
-    first_list.sort()
-    second_list.sort()
-    diff_list = [abs(second_list[i] - first_list[i]) for i in range(len(first_list))]
-    return sum(diff_list)
+        report = line.strip().split()
+        report = [int(num) for num in report]
+        trend = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+        safe = (all(num > 0 for num in trend) or all(num < 0 for num in trend)) and all(
+            abs(num) <= 3 for num in trend
+        )
+        if safe:
+            total_safe += 1
+    return total_safe
+
+
+def second_puzzle(filename):
+    """Red-Nosed Reports part 2"""
+    with open(filename, "r") as f:
+        data = f.readlines()
+    total_safe = 0
+    for line in data:
+        report = line.strip().split()
+        report = [int(num) for num in report]
+        trend = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+        safe = (all(num > 0 for num in trend) or all(num < 0 for num in trend)) and all(
+            abs(num) <= 3 for num in trend
+        )
+        if safe:
+            total_safe += 1
+    return total_safe
 
 
 if __name__ == "__main__":
@@ -22,7 +40,7 @@ if __name__ == "__main__":
     print(result)
     result = first_puzzle("day_02_input_a.txt")
     print(result)
-"""    result = second_puzzle("day_02_input_test.txt")
-    print(result)
-    result = second_puzzle("day_02_input_b.txt")
-    print(result)"""
+#    result = second_puzzle("day_02_input_test.txt")
+#    print(result)
+#    result = second_puzzle("day_02_input_b.txt")
+#    print(result)
