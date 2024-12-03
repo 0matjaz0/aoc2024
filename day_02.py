bcc95919053c433bf3a48bc28cd4d2e412344163
MV <matjaz.vencelj@ijs.si>
2024-12-03 12:03:22 +0100

    Finish day 2

--- day_02.py
+++ day_02.py
@@ -18,19 +18,32 @@ def first_puzzle(filename):
     return total_safe
 
 
+############################################
+
+
+def safety_test(report):
+    """Test if the report is safe"""
+    trend = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+    safe = (all(num > 0 for num in trend) or all(num < 0 for num in trend)) and all(
+        abs(num) <= 3 for num in trend
+    )
+    return safe
+
+
 def second_puzzle(filename):
     """Red-Nosed Reports part 2"""
     with open(filename, "r") as f:
         data = f.readlines()
     total_safe = 0
     for line in data:
+        repairable = False
         report = line.strip().split()
         report = [int(num) for num in report]
-        trend = [report[i + 1] - report[i] for i in range(len(report) - 1)]
-        safe = (all(num > 0 for num in trend) or all(num < 0 for num in trend)) and all(
-            abs(num) <= 3 for num in trend
-        )
-        if safe:
+        for i in range(len(report)):
+            if safety_test(report[:i] + report[i + 1 :]):
+                repairable = True
+                break
+        if repairable:
             total_safe += 1
     return total_safe
 
@@ -40,7 +53,9 @@ if __name__ == "__main__":
     print(result)
     result = first_puzzle("day_02_input_a.txt")
     print(result)
-#    result = second_puzzle("day_02_input_test.txt")
-#    print(result)
-#    result = second_puzzle("day_02_input_b.txt")
-#    print(result)
+    result = second_puzzle("day_02_input_test.txt")
+    print(result)
+    result = second_puzzle("day_02_input_a.txt")
+    print(result)
+
+############################################
