c2d8dc2c5e3dd02840bcfe9c4b7d2bf0a965639a
MV <matjaz.vencelj@ijs.si>
2024-12-03 10:36:07 +0100

    Fix multiplication bug

--- day_01.py
+++ day_01.py
@@ -1,3 +1,5 @@
+"""AOC 2024 day 1"""
+
 def first_puzzle(filename):
     """Historian Hysteria part 1"""
     with open(filename, "r") as f:
@@ -24,12 +26,14 @@ def second_puzzle(filename):
         numbers = line.strip().split()
         first_list.append(int(numbers[0]))
         second_list.append(int(numbers[1]))
-    similarity_list = [second_list.count(first_list[i]) for i in range(len(first_list))]
+    similarity_list = [second_list.count(first_list[i]) * first_list[i] \
+                       for i in range(len(first_list))]
     return sum(similarity_list)
 
 if __name__ == "__main__":
     result = first_puzzle("day_01_input_a.txt")
     print(result)
+    # result = second_puzzle("day_01_input_test.txt")
     result = second_puzzle("day_01_input_b.txt")
     print(result)
 
