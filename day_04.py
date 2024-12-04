831a8f4b0120b7c24d347b9f15e7a83acd3bbe9e
MV <matjaz.vencelj@ijs.si>
2024-12-04 08:57:48 +0100

    Write part 1 of day 4

--- day_04.py
+++ day_04.py
@@ -1,49 +1,61 @@
-"""AOC 2024 day 3"""
+"""AOC 2024 day 4"""
 
 import re
 
 
+def count_xmas(strings):
+    count = 0
+    for string in strings:
+        count += string.count("XMAS") + string.count("SAMX")
+    return count
+
+
 def first_puzzle(filename):
-    """Mull It Over part 1"""
+    """Ceres Search part 1"""
+    total = 0
     with open(filename, "r") as f:
         data = f.readlines()
-    sum_of_products = 0
+    table = []
     for line in data:
-        line
-        allcmds = re.finditer(r"mul\((\d{1,3})\,(\d{1,3})\)", line)
-        for cmd in allcmds:
-            a, b = int(cmd.group(1)), int(cmd.group(2))
-            sum_of_products += a * b
-    return sum_of_products
+        line = line.strip()
+        letters = list(line)
+        table.append(letters)
+    maxx, maxy = len(table[0]), len(table)
+    m_table = table[::-1]  # mirror table
+    #    print(maxx, maxy)
+    diagonals = []
+    m_diagonals = []
+    for i in range(maxx + maxy - 1):
+        diagonal = []
+        m_diagonal = []
+        for j in range(max(maxx, maxy)):
+            if i - j >= 0 and i - j < maxy and j < maxx:
+                diagonal.append(table[i - j][j])
+                m_diagonal.append(m_table[i - j][j])
+        diagonal_str = "".join(diagonal)
+        diagonals.append(diagonal_str)
+        m_diagonal_str = "".join(m_diagonal)
+        m_diagonals.append(m_diagonal_str)
+    increase = count_xmas(diagonals)
+    m_increase = count_xmas(m_diagonals)
+    total = increase + m_increase
+    row_strings = ["".join(row) for row in table]
+    total += count_xmas(row_strings)
+    column_strings = ["".join(column) for column in zip(*table)]
+    total += count_xmas(column_strings)
+
+    return total
 
 
-def second_puzzle(filename):
-    """Mull It Over part 2"""
-    with open(filename, "r") as f:
-        data = f.readlines()
-    sum_of_products = 0
-    enabled = True
-    for line in data:
-        allcmds = re.finditer(r"mul\((\d{1,3})\,(\d{1,3})\)|do\(\)|don't\(\)", line)
-        for cmd in allcmds:
-            if cmd.group(0) == "do()":
-                enabled = True
-            elif cmd.group(0) == "don't()":
-                enabled = False
-            elif enabled:
-                a, b = int(cmd.group(1)), int(cmd.group(2))
-                sum_of_products += a * b
-    return sum_of_products
+#    for i in range(len(diagonals)):
+#        print(diagonals[i])
 
 
 if __name__ == "__main__":
-    result = first_puzzle("day_03_input_test.txt")
+    result = first_puzzle("day_04_input_test.txt")
     print(result)
-    result = first_puzzle("day_03_input_a.txt")
-    print(result)
-    result = second_puzzle("day_03_input_test2.txt")
-    print(result)
-    result = second_puzzle("day_03_input_a.txt")
-    print(result)
+
+#    result = first_puzzle("day_04_input_a.txt")
+#  print(result)
 
 ############################################
