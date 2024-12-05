d00afc77a6fa9157a201c80284dd547697d491f5
MV <matjaz.vencelj@ijs.si>
2024-12-05 09:57:04 +0100

    Finish day 5 part 1

--- day_05.py
+++ day_05.py
@@ -19,26 +19,29 @@ def read_and_parse(filename):
     return rules, prints
 
 
-def check_print(rules, print):
+def check_print(rules, print_):
     """Check if the print is valid"""
-    for index, page in enumerate(print):
-        # do something
-    return False
-
+    for index, page1 in enumerate(print_):
+        for page2 in print_[index + 1 :]:
+            if [page2, page1] in rules:
+                return False
+    return True
 
 
 def first_puzzle(filename):
     """Print Queue part 1"""
     rules, prints = read_and_parse(filename)
-    print(rules)
-    print(prints)
-    return 0
+    total = 0
+    for print_ in prints:
+        if check_print(rules, print_):
+            total += print_[len(print_) // 2]
+    return total
 
 
 if __name__ == "__main__":
     result = first_puzzle("day_05_input_test.txt")
     print(result)
-#    result = first_puzzle("day_05_input_a.txt")
-#    print(result)
+    result = first_puzzle("day_05_input_a.txt")
+    print(result)
 
 ############################################
