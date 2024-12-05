c55b8c9413da526560a87b9da4780eb77c3f3845
MV <matjaz.vencelj@ijs.si>
2024-12-05 08:22:30 +0100

    Start day 5

---
+++ day_05.py
@@ -0,0 +1,20 @@
+"""AOC 2024 day 4"""
+
+
+def first_puzzle(filename):
+    """Ceres Search part 1"""
+    with open(filename, "r") as f:
+        data = f.readlines()
+    table = []
+    for line in data:
+        pass
+    return 0
+
+
+if __name__ == "__main__":
+    result = first_puzzle("day_05_input_test.txt")
+    print(result)
+#    result = first_puzzle("day_05_input_a.txt")
+#    print(result)
+
+############################################
