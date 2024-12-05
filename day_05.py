bd12a745950f24ca706dbcec8fb5181d404662fd
MV <matjaz.vencelj@ijs.si>
2024-12-05 10:38:55 +0100

    Finish day 5

--- day_05.py
+++ day_05.py
@@ -28,20 +28,37 @@ def check_print(rules, print_):
     return True
 
 
-def first_puzzle(filename):
-    """Print Queue part 1"""
+def reorder(print_, rules):
+    """Reorder the print"""
+    order_ok = False
+    while not order_ok:
+        order_ok = True
+        for index, page1 in enumerate(print_):
+            for page2 in print_[index + 1 :]:
+                if [page2, page1] in rules:
+                    print_[index], print_[index + 1] = print_[index + 1], print_[index]
+                    order_ok = False
+                    break
+    return print_
+
+
+def puzzles(filename):
+    """Print Queue part 1 and 2"""
     rules, prints = read_and_parse(filename)
-    total = 0
+    total_good = total_bad = 0
     for print_ in prints:
         if check_print(rules, print_):
-            total += print_[len(print_) // 2]
-    return total
+            total_good += print_[len(print_) // 2]
+        else:
+            reordered = reorder(print_, rules)
+            total_bad += reordered[len(reordered) // 2]
+    return total_good, total_bad
 
 
 if __name__ == "__main__":
-    result = first_puzzle("day_05_input_test.txt")
+    result = puzzles("day_05_input_test.txt")
     print(result)
-    result = first_puzzle("day_05_input_a.txt")
+    result = puzzles("day_05_input_a.txt")
     print(result)
 
 ############################################
